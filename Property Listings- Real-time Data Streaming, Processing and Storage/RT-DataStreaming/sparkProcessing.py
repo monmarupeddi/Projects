ec5b0f9429c6a0205fe3b787c6cec328590dadbf
MounicaM <97743227+monmarupeddi@users.noreply.github.com>
2024-09-04 12:24:50 -0500

    Update sparkProcessing.py

--- Property Listings- Real-time Data Streaming, Processing and Storage/RT-DataStreaming/sparkProcessing.py
+++ Property Listings- Real-time Data Streaming, Processing and Storage/RT-DataStreaming/sparkProcessing.py
@@ -2,19 +2,10 @@ import logging
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
-
-def create_keyspace(session):
-    session.execute("""
-        CREATE KEYSPACE IF NOT EXISTS spark_stream
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
-    """)
-
-    print("Keyspace created successfully!")
-
-
-def create_table(session):
+# create a cassandra table to populate with data
+def createTable(session):
     session.execute("""
     CREATE TABLE IF NOT EXISTS spark_stream.property_listings (
     id UUID PRIMARY KEY,
@@ -29,20 +20,20 @@ def create_table(session):
     type TEXT,
     featured TEXT,
     non_owner_occupied TEXT,
-    area TEXT,
-    price TEXT,
+    area FLOAT,
+    price FLOAT,
     rent_estimate TEXT,
-    taxVal TEXT,
+    taxVal FLOAT,
     lat TEXT,
     long TEXT,
     image TEXT);""")
 
-    print("Table created successfully!")
+    print("Table created")
 
-
-def insert_data(session, **kwargs):
-    print("inserting data...")
-
+# insert into cassandra
+def insertDataIntoCassandra(session, **kwargs):
+    print("Inserting---")
+# get the data from arguments in a dictionary form.
     bed = kwargs.get('bed')
     bath = kwargs.get('bath')
     street = kwargs.get('street')
@@ -61,10 +52,10 @@ def insert_data(session, **kwargs):
     lat = kwargs.get('lat')
     long = kwargs.get('long')  # Using 'long_' to avoid conflict with the built-in 'long' type in Python 2.x
     image = kwargs.get('image')
-
+# execute query
     try:
         session.execute("""
-        
+       
     INSERT INTO spark_streams.property_listings (
         bed, bath, street, city, country, zip, days_on_zillow, 
         status, type, featured, non_owner_occupied, area, 
@@ -76,32 +67,32 @@ def insert_data(session, **kwargs):
         logging.info(f"Data inserted")
 
     except Exception as e:
-        logging.error(f'could not insert data due to {e}')
+        logging.error("Insertion unsuccessful due to {e}")
 
-
-def create_spark_connection():
-    s_conn = None
+# spark connection
+def createSparkSession():
+    connection = None
 
     try:
-        s_conn = SparkSession.builder \
+        connection = SparkSession.builder \
             .appName('SparkDataStreaming') \
             .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
                                            "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
 
-        s_conn.sparkContext.setLogLevel("ERROR")
+        connection.sparkContext.setLogLevel("ERROR")
         logging.info("Spark connection created successfully!")
     except Exception as e:
         logging.error(f"Couldn't create the spark session due to exception {e}")
 
-    return s_conn
+    return connection
 
-
-def connect_to_kafka(spark_conn):
+# kafka connection: insert into spark dataframe the data coming from kafka
+def connect2kafka(spark_connection):
     spark_df = None
     try:
-        spark_df = spark_conn.readStream \
+        spark_df = spark_connection.readStream \
             .format('kafka') \
             .option('kafka.bootstrap.servers', 'localhost:9092') \
             .option('subscribe', 'users_created') \
@@ -109,12 +100,12 @@ def connect_to_kafka(spark_conn):
             .load()
         logging.info("kafka dataframe created successfully")
     except Exception as e:
-        logging.warning(f"kafka dataframe could not be created because: {e}")
+        logging.warning(f"error: {e}")
 
     return spark_df
 
-
-def create_cassandra_connection():
+# connect to cassandra
+def createCassandraConnection():
     try:
         # connecting to the cassandra cluster
         cluster = Cluster(['localhost'])
@@ -123,8 +114,9 @@ def create_cassandra_connection():
 
         return cas_session
     except Exception as e:
-        logging.error(f"Could not create cassandra connection due to {e}")
+        logging.error(f"cassandra connection unsuccessful due to {e}")
         return None
+
 
 def create_selection_df_from_kafka(spark_df):
     schema = StructType([
@@ -140,9 +132,9 @@ def create_selection_df_from_kafka(spark_df):
         StructField("featured", StringType(), False),
         StructField("non_owner_occupied", StringType(), False),
         StructField("area", StringType(), False),
-        StructField("price", StringType(), False),
+        StructField("price", IntegerType(), False),
         StructField("rent_estimate", StringType(), False),
-        StructField("taxVal", StringType(), False),
+        StructField("taxVal", IntegerType(), False),
         StructField("lat", StringType(), False),
         StructField("long", StringType(), False),
         StructField("image", StringType(), False)
@@ -156,25 +148,44 @@ def create_selection_df_from_kafka(spark_df):
 
 
 if __name__ == "__main__":
-    # create spark connection
-    spark_conn = create_spark_connection()
 
-    if spark_conn is not None:
-        # connect to kafka with spark connection
-        spark_df = connect_to_kafka(spark_conn)
+    spark_connection = createSparkSession()
+
+    # Proceed if the Spark session was created successfully
+    if spark_connection is not None:
+        # Connect to Kafka and create a DataFrame from the Kafka stream
+        spark_df = connect2kafka(spark_connection)
+
+        # Transform the Kafka DataFrame by applying the schema to parse the JSON data
         selection_df = create_selection_df_from_kafka(spark_df)
-        session = create_cassandra_connection()
 
+        # Establish a connection to the Cassandra database
+        session = createCassandraConnection()
+
+        # Proceed if the Cassandra connection was successfully established
         if session is not None:
-            create_keyspace(session)
-            create_table(session)
+            # Create a keyspace in Cassandra if it does not already exist
+            session.execute("""
+                CREATE KEYSPACE IF NOT EXISTS spark_stream
+                WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+            """)
+            print("Keyspace created successfully!")
 
+            # Create the table within the Cassandra keyspace if it does not already exist
+            createTable(session)
+
+            # Log the start of the streaming process
             logging.info("Streaming is being started...")
 
-            streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
-                               .option('checkpointLocation', '/tmp/checkpoint')
-                               .option('keyspace', 'spark_stream')
-                               .option('table', 'property_listings')
-                               .start())
+            # Start a streaming query that reads from the transformed Kafka DataFrame
+            # and writes the data to the Cassandra table
+            streaming_query = (selection_df.writeStream
+                               .format("org.apache.spark.sql.cassandra")
+                               .option('checkpointLocation',
+                                       '/tmp/checkpoint')  # Checkpoint directory for fault tolerance
+                               .option('keyspace', 'spark_stream')  # Cassandra keyspace
+                               .option('table', 'property_listings')  # Cassandra table
+                               .start())  # Start the streaming query
 
+            # Wait for the streaming query to finish
             streaming_query.awaitTermination()
